2bcb50b87f907c7ef3f422caa9aa85291f3dc914
SourcesNet <SourcesNet@DESKTOP-G7HPCOD>
2024-09-30 01:05:31 +0200

    Add project files.

---
+++ javaTraning-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace javaTraning_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
